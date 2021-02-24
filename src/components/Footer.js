8eab297eefdb90a113a26b2f98fb693c8d18230e
piotr13 <piotrek.lubiecki13@gmail.com>
2021-02-24 19:59:09 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <span>The Odin Project | CV Application</span>
+        <span>Piotr Lubiecki | 2021</span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
